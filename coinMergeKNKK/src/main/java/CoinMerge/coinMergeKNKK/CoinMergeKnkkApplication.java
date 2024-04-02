95f1027776bf3d6be5832b7cbe1d2d8e5db94215
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2024-04-02 18:56:43 +0900

    Add files via upload

---
+++ coinMergeKNKK/src/main/java/CoinMerge/coinMergeKNKK/CoinMergeKnkkApplication.java
@@ -0,0 +1,13 @@
+package CoinMerge.coinMergeKNKK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinMergeKnkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinMergeKnkkApplication.class, args);
+	}
+
+}
---
+++ coinMergeKNKK/src/test/java/CoinMerge/coinMergeKNKK/CoinMergeKnkkApplicationTests.java
@@ -0,0 +1,13 @@
+package CoinMerge.coinMergeKNKK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinMergeKnkkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
