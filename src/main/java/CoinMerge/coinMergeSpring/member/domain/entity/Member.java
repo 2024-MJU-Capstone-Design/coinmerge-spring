98785c9600beddf76d4836485a01fea88c674ba2
meohyun2 <eogus6242@gmail.com>
2024-04-16 03:29:20 +0900

    add: 멤버 레포지토리 및 엔티티

---
+++ src/main/java/CoinMerge/coinMergeSpring/member/domain/entity/Member.java
@@ -0,0 +1,68 @@
+package CoinMerge.coinMergeSpring.member.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(length = 36)
+  private String id;
+
+  @Column(length = 64, nullable = false, unique = true)
+  private String email;
+
+  @Column(length = 128, nullable = false)
+  private String password;
+
+  @Column(length = 128, nullable = false)
+  private String salt;
+
+  @Column(length = 32)
+  private String nickname;
+
+  @Column(name = "profile_image_uri")
+  private String profileImageUri;
+
+  private String description;
+
+  @CreationTimestamp
+  @Column(length = 20, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(length = 20)
+  private LocalDateTime updatedAt;
+
+  @Override
+  public String toString() {
+    return "Member{" +
+        "id='" + id + '\'' +
+        ", email='" + email + '\'' +
+        ", password='" + password + '\'' +
+        ", salt='" + salt + '\'' +
+        ", nickname='" + nickname + '\'' +
+        ", profileImageUri='" + profileImageUri + '\'' +
+        ", description='" + description + '\'' +
+        ", createdAt=" + createdAt +
+        ", updatedAt=" + updatedAt +
+        '}';
+  }
+}
---
+++ src/main/java/CoinMerge/coinMergeSpring/member/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package CoinMerge.coinMergeSpring.member.domain.repository;
+
+import CoinMerge.coinMergeSpring.member.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+  Member findByEmail(final String email);
+}
