c2e9912fc093b3c88d3c2014a9a89cdb1cef7f60
meohyun2 <eogus6242@gmail.com>
2024-05-13 15:35:59 +0900

    fix: 멤버 엔티티 내 파싱 함수 추가

--- src/main/java/CoinMerge/coinMergeSpring/member/dto/MemberResponse.java
+++ src/main/java/CoinMerge/coinMergeSpring/member/dto/MemberResponse.java
@@ -1,5 +1,6 @@
 package CoinMerge.coinMergeSpring.member.dto;
 
+import CoinMerge.coinMergeSpring.member.domain.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +9,17 @@ import lombok.RequiredArgsConstructor;
 @Builder
 @RequiredArgsConstructor
 public class MemberResponse {
+
   private final String id;
   private final String email;
   private final String description;
   private final String profileImageUri;
   private final String nickname;
+
+  public static MemberResponse toMemberResponse(Member member) {
+    return MemberResponse.builder().id(member.getId()).email(member.getEmail())
+        .nickname(member.getNickname()).description(member.getDescription())
+        .profileImageUri(member.getProfileImageUri()).build();
+  }
+
 }
